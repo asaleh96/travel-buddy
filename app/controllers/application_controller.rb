b65bf9c9d13bfa34480aea6118de5f25998cdeb0
Shahid Farooq <sif101@hotmail.co.uk>
2023-01-25 03:57:36 +0000

    fixed interest user form and minor fixes to schema

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :gender, :date_of_birth, :location])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :gender, :date_of_birth, :location, interest_ids: []])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :gender, :date_of_birth, :location])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,9 @@
 class UsersController < ApplicationController
-
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    if @user.update(user_params)
-      redirect_to user_path
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :username, :gender, :date_of_birth, :location,
-                                 :email, :password)
-  end
-
-  def set_user
     @user = User.find(params[:id])
   end
 end
